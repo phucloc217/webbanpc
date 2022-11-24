f9d148d7e60a099c30753525c072b1be335b0369
phucloc217 <loc.nguyen456789@gmail.com>
2022-11-24 13:11:39 +0700

    Chuc nang dang nhap

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,5 +29,9 @@ class UserController extends Controller
             }
 
         }
+        else
+        {
+            return redirect()->back()->withErrors(['msg' => 'Sai tên đăng nhập']); 
+        }
     }
 }
